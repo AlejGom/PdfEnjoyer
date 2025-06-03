e1c75e6aa1a6e0bfa81149a74e2da823b87ef1ab
AlejGom <alejgom02@gmail.com>
2025-06-03 10:02:26 +0000

    First commit Laravel Project

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ContratoController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/saludo', [ContratoController::class, 'mostrarSaludo']);
+
