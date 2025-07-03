3c7feb32aaea22c1fa30e18e82cc3f23cc57ef4f
AlejGom <alejgom02@gmail.com>
2025-07-03 16:11:05 +0200

    choice user view create (progress paused)

---
+++ resources/views/choiceuser.blade.php
@@ -0,0 +1,39 @@
+@extends('templates.layout')
+
+@section('content')
+
+<link rel="stylesheet" href="{{ asset('css/index.css') }}">
+
+<h1>¿Quien eres? Identifícate</h1>
+
+<!-- 
+TODO añadir selección de usuarios en base a los usuarios que existen
+ en la base de datos, estos podrán ser añadidos y eliminados por
+ el usuario con el rol "admin". Añadir tabla de usuarios y darles rol.
+ -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+@endsection
